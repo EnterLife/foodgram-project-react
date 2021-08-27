a74aa08a3286166d0d251d07e3ad55b4951dbb14
EnterLife <babeshkodo@gmail.com>
2021-08-27 14:56:07 +0300

    Commit

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from users.views import FollowViewSet
 
 from .views import (DownloadShoppingCart, FavouriteViewSet, IngredientViewSet,
                     RecipesViewSet, ShoppingListViewSet, TagsViewSet)
@@ -8,7 +9,7 @@ router = DefaultRouter()
 router.register('tags', TagsViewSet, basename='tags')
 router.register('recipes', RecipesViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
-
+router.register('users', FollowViewSet, basename='users')
 
 urlpatterns = [
     path('recipes/<int:recipe_id>/favorite/',
