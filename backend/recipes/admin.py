509ea4f787c02326db4d406f187ebdbbc4b8d7fe
EnterLife <babeshkodo@gmail.com>
2021-08-27 13:51:47 +0300

    Commit

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,35 +1,23 @@
 from django.contrib import admin
-from import_export.admin import ImportMixin
 
-from .models import Favorite, Ingredient, IngredientForRecipe, Recipe, Tag
-from .resources import IngredientResource
-
-
-class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipe', 'user')
-
-
-class IngredientForRecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ingredient', 'recipe', 'amount')
+from .models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'name',)
     list_filter = ('author', 'name', 'tags')
+    list_display = ('name', 'followers')
+
+    @admin.display(empty_value=None)
+    def followers(self, obj):
+        return obj.favorite_recipe.all().count()
 
 
-class IngredientAdmin(ImportMixin, admin.ModelAdmin):
-    list_filter = ('id', 'name', 'measurement_unit',)
-    search_fields = ('name',)
-    resource_class = IngredientResource
+class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
 
 
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'color', 'slug')
-
-
-admin.site.register(IngredientForRecipe, IngredientForRecipeAdmin)
-admin.site.register(Tag, TagAdmin)
+admin.site.register(Tag)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,14 +4,8 @@ from .models import CustomUser, Follow
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'username', 'first_name', 'last_name')
-    list_filter = ('email', 'username')
-
-
-class FollowAdmin(admin.ModelAdmin):
-    list_display = ('author', 'user', 'created_at')
-    list_filter = ('created_at',)
+    list_filter = ('username', 'email')
 
 
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Follow, FollowAdmin)
+admin.site.register(Follow)
